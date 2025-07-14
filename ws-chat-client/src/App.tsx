9bd019b84563da5cd12ecdc985a7be1f00f9be11
Venki1402 <saivenkatesh.alampally@gmail.com>
2025-07-14 09:38:53 +0530

    feat: chat input ui

--- ws-chat-client/src/App.tsx
+++ ws-chat-client/src/App.tsx
@@ -2,7 +2,8 @@ import ChatInput from "./components/chat-input";
 
 const App = () => {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
+      <div className="flex-1"></div>
       <ChatInput />
     </div>
   );
--- ws-chat-client/src/components/chat-input.tsx
+++ ws-chat-client/src/components/chat-input.tsx
@@ -1,11 +1,21 @@
+import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 
 const ChatInput = () => {
   return (
-    <div className="flex items-center justify-between p-4">
-      <Textarea />
-      <Button />
+    <div className="flex items-end gap-2 p-4">
+      <div className="flex-1 flex flex-col gap-1">
+        <Label htmlFor="message" className="sr-only">
+          Your message
+        </Label>
+        <Textarea
+          placeholder="Type your message here."
+          id="message"
+          className="resize-none min-h-[40px] max-h-[120px] w-full"
+        />
+      </div>
+      <Button className="h-10 px-4">Send</Button>
     </div>
   );
 };
